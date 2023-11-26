272826aa9c24c52714b185755351c920af437890
anisa <aniso1020@gmail.com>
2023-11-26 20:43:34 +0100

    Legg til ReadMe innhold

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # DS3103-webutvikling-2023
+
+# Speed Master Formula 1
+
+**🏎️ Welcome to Speed Master F1 – Your Extraordinary Formula 1 Experience Begins Here! 🏁**
+
+## **Table of Contents**
+
+- [About](https://chat.openai.com/c/9942b749-de03-4371-9b6b-44b38e294751#about)
+- [Key Features](https://chat.openai.com/c/9942b749-de03-4371-9b6b-44b38e294751#key-features)
+- [Installation](https://chat.openai.com/c/9942b749-de03-4371-9b6b-44b38e294751#installation)
+- [Usage](https://chat.openai.com/c/9942b749-de03-4371-9b6b-44b38e294751#usage)
+- [Technologies Used](https://chat.openai.com/c/9942b749-de03-4371-9b6b-44b38e294751#technologies-used)
+- [Contributing](https://chat.openai.com/c/9942b749-de03-4371-9b6b-44b38e294751#contributing)
+- [License](https://chat.openai.com/c/9942b749-de03-4371-9b6b-44b38e294751#license)
+
+## **About**
+
+Speed Master F1 is a full-stack web application dedicated to Formula 1 enthusiasts. It offers a comprehensive platform to manage driver details, team information, race records, and challenges your knowledge with engaging quizzes.
+
+## **Key Features**
+
+1. **Driver Database 🚀**
+   - CRUD Operations: Manage your favorite drivers effortlessly.
+   - Comprehensive Information: Access details such as name, age, and nationality.
+2. **Team Management 🏁**
+   - Build Your Dream Team: Explore and organize teams with ease.
+   - Driver Lineup: Track and update the driver lineup for each team.
+3. **Race Records 🌍**
+   - CRUD Operations: Keep tabs on every race with winner details, location, and winning time.
+   - Stay Updated: Always know who's dominating the tracks.
+4. **Challenge Your Knowledge 🏆**
+   - Driver Quiz: Test your knowledge about F1 drivers.
+   - Team Quiz: Prove your expertise on F1 teams.
+
+## **Installation**
+
+1. **Navigate to Project Directory:**
+
+   ```bash
+   cd DS3103-Webutvikling-2023
+   cd ReactApplication
+   cd WebAPI
+   ```
+
+2. **Run Backend**
+
+   ```bash
+   cd DS3103-Webutvikling-2023/WebAPI
+   dotnet run
+   ```
+
+3. **Run Frontend** 
+
+   ```bash
+   cd DS3103-Webutvikling-2023/ReactApplication
+   npm install
+   npm run dev
+   ```
+
+## **Usage**
+
+1. **Access the App:**
+   Open your browser and go to [http://localhost:3000](http://localhost:3000/)
+
+## **Technologies Used**
+
+- Frontend:
+  - [React](https://reactjs.org/),
+  - React Hooks
+  - Vite Server
+  - Material UI
+  - Boostrap 5
+- Backend: .Net, APi
+- Database: Sqlite
--- ReactApplication/src/pages/Home.jsx
+++ ReactApplication/src/pages/Home.jsx
@@ -9,33 +9,34 @@ export function Home() {
           <div class="col-md-6">
             <div class="card text-center">
               <div class="card-body">
-                <h5 class="card-title">Utforsk Formel 1-lagene!</h5>
+                <h5 class="card-title">Explore the Formula 1 teams!</h5>
                 <p class="card-text">
-                  Formel 1 er ikke bare en kamp mellom førere, men også en
-                  konkurranse mellom de ledende racingteamene. To av de mest
-                  ikoniske lagene er Red Bull Racing og Ferrari. Er du klar for
-                  å teste dine ferdigheter og lære mer om de ledende Formel
-                  1-lagene? Start quizen og nyt reisen gjennom spenningen i
-                  F1-teamenes univers!
+                  Speed Master F1 is not just a battle among drivers but also a
+                  competition between the leading racing teams. Two of the most
+                  iconic teams are Red Bull Racing and Ferrari. Are you ready to
+                  test your skills and learn more about the top Speed Master F1
+                  teams? Start the quiz and enjoy the journey through the
+                  excitement of the F1 teams' universe!
                 </p>
                 <Link to="/teams-quiz" class="btn btn-outline-primary">
-                Spill Teams Quiz
+                  Play Teams Quiz
                 </Link>
               </div>
             </div>
           </div>
-          <div class="col-md-6">
+          <div class="col-md-6 ">
             <div class="card text-center">
               <div class="card-body">
-                <h5 class="card-title">Velkommen til Drivers F1 Quiz!</h5>
+                <h5 class="card-title">Welcome to Drivers F1 Quiz!</h5>
                 <p class="card-text">
-                  Utforsk din kunnskap om dagens Formel 1-stjerner. Test deg
-                  selv med spørsmål om førere som Max Verstappen, Sergio Perez,
-                  Lewis Hamilton, Carlos Sainz, og Fernando Alonso. Skynd deg og
-                  start quizen nå for en opplevelse fylt med moro og spenning!
+                  Explore your knowledge about today's Formula 1 stars. Test
+                  yourself with questions about drivers like Max Verstappen,
+                  Sergio Perez, Lewis Hamilton, Carlos Sainz, and Fernando
+                  Alonso. Hurry and start the quiz now for an experience filled
+                  with fun and excitement!
                 </p>
                 <Link to="/drivers-quiz" class="btn btn-outline-primary">
-                  Spill Drivers Quiz
+                  Play Drivers Quiz
                 </Link>
               </div>
             </div>
